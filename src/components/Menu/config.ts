94a490a23563f17dc6478edf4ad2bfeabb7a0da4
pooltoken <pooltokenfinance@gmail.com>
2021-03-01 18:12:04 +0100

    initial

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,103 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://pooltoken.finance/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Base farms',
+    icon: 'FarmIcon',
+    href: 'https://pooltoken.finance/base-farms',
+  },
+  {
+    label: 'BSC pools',
+    icon: 'BscIcon',
+    href: 'https://pooltoken.finance/bsc-pools',
+  },
+  {
+    label: 'Base Pools',
+    icon: 'PoolIcon',
+    href: 'https://pooltoken.finance/base-pools',
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://pooltoken.finance/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: 'https://pooltoken.finance/nft',
+  // },
+  // {
+  //   label: 'Teams & Profile',
+  //   icon: 'GroupsIcon',
+  //   calloutClass: 'rainbow',
+  //   items: [
+  //     {
+  //       label: 'Leaderboard',
+  //       href: 'https://pooltoken.finance/teams',
+  //     },
+  //     {
+  //       label: 'Task Center',
+  //       href: 'https://pooltoken.finance/profile/tasks',
+  //     },
+  //     {
+  //       label: 'Your Profile',
+  //       href: 'https://pooltoken.finance/profile',
+  //     },
+  //   ],
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'Overview',
+        href: 'https://pooltokens.info',
+      },
+      {
+        label: 'Github',
+        href: 'https://github.com/pooltoken',
+      },
+    ],
+  },
+  // {
+  //   label: 'IFO',
+  //   icon: 'IfoIcon',
+  //   href: 'https://pancakeswap.finance/ifo',
+  // },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Docs',
+        href: 'https://pooltoken.finance',
+      },
+      {
+        label: 'Blog',
+        href: 'https://pooltoken.finance',
+      },
+    ],
+  },
+]
+
+export default config
---
+++ src/constants/priceContracts.ts
@@ -0,0 +1,7 @@
+const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
+    cakeAddress: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+    busdAddress: '0x0ed8e0a2d99643e1e65cca22ed4424090b8b7458',
+    lpAddress: '0x90b253b4e869877f523316a427c494193ffc88ac'
+  }
+  
+  export default priceContracts
